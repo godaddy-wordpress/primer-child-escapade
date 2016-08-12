9d5d138939954401bda3f665bc3928abed6623f5
Jonathan Bardo <bardo.jonathan@gmail.com>
2016-08-12 15:49:03 -0400

    Remove deprecated tags

--- functions.php
+++ functions.php
@@ -150,8 +150,8 @@ function escapade_sidebars( $sidebars ) {
 	unset( $sidebars['sidebar-2'] );
 
 	$sidebars['hero'] = array(
-		'name'          => esc_html__( 'Hero', 'ascension' ),
-		'description'   => esc_html__( 'Hero widgets appear over the header image on the front page.', 'ascension' ),
+		'name'          => esc_html__( 'Hero', 'escapade' ),
+		'description'   => esc_html__( 'Hero widgets appear over the header image on the front page.', 'escapade' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h2 class="widget-title">',
